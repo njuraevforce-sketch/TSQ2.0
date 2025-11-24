3cf762fc8d4248d5751fe69e7dd965a8624a0f8c
njuraevforce-sketch <n.juraevforce@gmail.com>
2025-11-24 08:16:30 +0500

    Delete static/js directory

--- static/js/chunk-vendors.d9d12323.js
+++
@@ -1,8 +0,0 @@
-// Минимальная версия vendors файла
-(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["chunk-vendors"], {
-    "00c2": function(t, e, n) {
-        "use strict";
-        // ... остальной код vendors.js из предоставленного примера
-        // Этот файл должен содержать все зависимости Vue.js и UniApp
-    }
-}]);

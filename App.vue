ea8a6be2906e38e34b1bd16e4a40b0cd55ffa284
njuraevforce-sketch <n.juraevforce@gmail.com>
2025-11-25 04:39:51 +0500

    Update App.vue

--- App.vue
+++ App.vue
@@ -1,17 +1,37 @@
 <script>
 export default {
-  onLaunch: function() {
-    console.log('App Launch')
-  },
-  onShow: function() {
-    console.log('App Show')
-  },
-  onHide: function() {
-    console.log('App Hide')
-  }
+    onLaunch: function() {
+        console.log('App Launch');
+        // Проверка авторизации
+        this.checkAuth();
+    },
+    onShow: function() {
+        console.log('App Show');
+    },
+    onHide: function() {
+        console.log('App Hide');
+    },
+    methods: {
+        checkAuth() {
+            // Проверка токена авторизации
+            const token = uni.getStorageSync('userToken');
+            if (!token) {
+                uni.redirectTo({
+                    url: '/pages/login/login'
+                });
+            }
+        }
+    }
 }
 </script>
 
 <style>
-/* Кастомные стили будут в uni.scss */
+/* Общие стили */
+page {
+    background: linear-gradient(180deg, #4e7771, #4e7771, rgba(185,215,168,0.75));
+    min-height: 100vh;
+}
+
+/* Импорт общих стилей из вашего CSS */
+@import url('./common/style.css');
 </style>

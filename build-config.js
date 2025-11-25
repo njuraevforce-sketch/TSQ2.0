b8bfeb1018337b2a035c76588433f2150b03eb53
njuraevforce-sketch <n.juraevforce@gmail.com>
2025-11-25 05:46:56 +0500

    Update build-config.js

--- build-config.js
+++ build-config.js
@@ -1,9 +1,7 @@
 const { build } = require('@dcloudio/uni-cli');
 
 module.exports = {
-  // Cloudflare Pages ожидает сборку в папку 'dist'
   outputDir: 'dist',
-  // Базовый путь для роутинга
   publicPath: './',
   configureWebpack: {
     plugins: []
